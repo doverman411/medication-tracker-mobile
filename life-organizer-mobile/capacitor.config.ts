b7f94cf9696085bca013976e0a4325a4bbc557b7
David Overman <portalerd@icloud.com>
2025-04-01 13:52:48 -0700

    Migrate to Angular 19

--- life-organizer-mobile/capacitor.config.ts
+++ life-organizer-mobile/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'life-organizer-mobile',
+  appName: 'LOM19',
   webDir: 'www'
 };
 
